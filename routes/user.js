fe4a05e4e36f02ecfdf6dd3809673d127535eb79
Glitch (glitchchord-app) <none>
2020-07-30 17:18:34 +0000

    💈🍵 Checkpoint ./routes/user.js:1025089/56:5152800/16

--- routes/user.js
+++ routes/user.js
@@ -99,6 +99,7 @@ router.get("/:id", middleware.isLogedIn, (req, res, next) => {
           }
         })
         .catch(e => {
+          // This is why the /@me page isn't working.
           console.log("I think this is the mother fucking bug....");
           res.send(e);
         });
