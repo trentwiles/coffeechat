cdc8a4ddfbadde215088bb998032d5778686e6c0
Glitch (glitchchord-app) <none>
2020-07-31 16:49:15 +0000

    👮🎐 Checkpoint ./public/js/unload.js:11113072/678 ./views/partials/header.ejs:11113072/264

--- public/js/unload.js
+++ public/js/unload.js
@@ -1,3 +1,11 @@
 function defreshUnload(f) {
-  alert("Loading "+f.replace(location.protocol+"//"+location.hostname, "")+"...");
+  if (f.replace(location.protocol + "//" + location.hostname, "") !== "/") {
+    alert(
+      "Loading " +
+        f.replace(location.protocol + "//" + location.hostname, "") +
+        "..."
+    );
+  } else {
+    alert("Going Home...");
+  }
 }
