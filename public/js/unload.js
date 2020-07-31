a9a14e9a0acd1471c91bf242a6913db0056f66a7
Glitch (glitchchord-app) <none>
2020-07-31 18:35:58 +0000

    🐸📌 Checkpoint ./views/joystick.ejs:11113072/311 ./public/js/unload.js:11113072/984

--- public/js/unload.js
+++ public/js/unload.js
@@ -1,6 +1,10 @@
 function defreshUnload(f) {
   if (f !== location.href) {
-    if (f.replace(location.protocol + "//" + location.hostname, "") !== "/") {
+    if (
+      f.replace(location.protocol + "//" + location.hostname, "") !== "/" &&
+      f.replace(location.protocol + "//" + location.hostname, "") !==
+        "/users/@me"
+    ) {
       alert(
         "Loading " +
           f.replace(location.protocol + "//" + location.hostname, "") +
