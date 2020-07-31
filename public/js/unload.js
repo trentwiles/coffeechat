cbc998dc68df222dc4e5bd44512df1f2bb7a0d61
Glitch (glitchchord-app) <none>
2020-07-31 17:18:38 +0000

    🎅🐥 Checkpoint ./public/js/unload.js:11113072/781 ./views/joystick.ejs:11113072/64

--- public/js/unload.js
+++ public/js/unload.js
@@ -1,11 +1,15 @@
 function defreshUnload(f) {
-  if (f.replace(location.protocol + "//" + location.hostname, "") !== "/") {
-    alert(
-      "Loading " +
-        f.replace(location.protocol + "//" + location.hostname, "") +
-        "..."
-    );
+  if (f !== location.href) {
+    if (f.replace(location.protocol + "//" + location.hostname, "") !== "/") {
+      alert(
+        "Loading " +
+          f.replace(location.protocol + "//" + location.hostname, "") +
+          "..."
+      );
+    } else {
+      alert("Going Home...");
+    }
   } else {
-    alert("Going Home...");
+    alert("Loading...");
   }
 }
