95d4874bfcf0a84969e4043b86f251f9bc79a7de
Glitch (glitchchord-app) <none>
2020-08-02 19:22:44 +0000

    ⛄️🕵 Checkpoint ./public/js/unload.js:11113072/1193

--- public/js/unload.js
+++ public/js/unload.js
@@ -1,13 +1,21 @@
 function defreshUnload(f) {
-  if (f !== location.href) {
+  if (f !== window.location.href) {
     if (
-      f.replace(location.protocol + "//" + location.hostname, "") !== "/" &&
-      f.replace(location.protocol + "//" + location.hostname, "") !==
-        "/users/@me"
+      f.replace(
+        window.location.protocol + "//" + window.location.hostname,
+        ""
+      ) !== "/" &&
+      f.replace(
+        window.location.protocol + "//" + window.location.hostname,
+        ""
+      ) !== "/users/@me"
     ) {
       alert(
         "Loading " +
-          f.replace(location.protocol + "//" + location.hostname, "") +
+          f.replace(
+            window.location.protocol + "//" + window.location.hostname,
+            ""
+          ) +
           "..."
       );
     } else {
