d01a5027a08a662c634c894c0d74d1dc8bfaa226
Glitch (glitchchord-app) <none>
2020-08-12 16:59:21 +0000

    💅🌜 Checkpoint ./models/channel.js:21023707/56 ./views/partials/header.ejs:21023707/436

--- models/channel.js
+++ models/channel.js
@@ -40,10 +40,7 @@ const channelSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
-  staffverify:{
-    type:Boolean,
-    default: false,
-  }
+ 
 });
 
 module.exports = mongoose.model("Channel", channelSchema);
