e83a5983058ed929adce0eb522dbe97658bf3a55
Glitch (glitchchord-app) <none>
2020-08-18 15:36:18 +0000

    💨🐩 Checkpoint ./views/marketplace.ejs:21023707/5912 ./routes/index.js:21023707/203 ./views/partials/channel_section.ejs:21023707/680 ./views/login.ejs:21023707/130 ./views/register.ejs:21023707/130 ./views/partials/header.ejs:21023707/9

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,13 @@ router.get("/joystick", (req, res)=>{
     res.redirect("/users/login")
   }
 })
-
+router.get("/market", (req, res)=>{
+  if(req.user){
+    res.render('marketplace',{title:"Marketplace"}) 
+  }else{
+    res.redirect("/users/login")
+  }
+})
 
 router.get("/discovery", (req, res)=>{
   if(req.user){
